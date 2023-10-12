0bd8e440f3fc04ff2cffcbd3ac2889b2cf726660
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-10-12 13:08:21 +0200

    RCrear interfaz DAO JPA para la entidad Libro 'BaseDatos3'

---
+++ src/main/java/com/example/crudspring/repository/BaseDatos3.java
@@ -0,0 +1,4 @@
+package com.example.crudspring.repository;
+
+public interface BaseDatos3 extends org.springframework.data.jpa.repository.JpaRepository<com.example.crudspring.bean.Libro, java.lang.Integer> {
+}
