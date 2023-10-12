1518ad3843435d8b2a78cb718401d134f1ee5dbf
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-10-12 10:00:38 +0200

    Agregado Controlador.java al paquete 'controllers'

---
+++ src/main/java/com/example/crudspring/controllers/Controlador.java
@@ -0,0 +1,7 @@
+package com.example.crudspring.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller // Anotación que indica que esta clase es un controlador de Spring
+public class Controlador {
+}
