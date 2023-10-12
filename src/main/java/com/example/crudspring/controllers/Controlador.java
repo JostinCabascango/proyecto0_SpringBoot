dca6d47a49f5e2b3953d6d78d388bd2e9144dfcd
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-10-12 10:07:18 +0200

    Añadimos 2 metoso handler al controlador ,(iniciar,login)

--- src/main/java/com/example/crudspring/controllers/Controlador.java
+++ src/main/java/com/example/crudspring/controllers/Controlador.java
@@ -1,7 +1,22 @@
 package com.example.crudspring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller // Anotación que indica que esta clase es un controlador de Spring
+@RequestMapping("") // Anotación que indica que este controlador responde a la raíz del servidor
 public class Controlador {
+    @GetMapping("/")
+    // Responde a la petición GET de la raíz del servidor y devuelve la vista login (login.html)
+    public String iniciar() {
+        return "login";
+    }
+
+    // Responde a la petición POST de la raíz del servidor y devuelve la vista consulta (consulta.html)
+    @PostMapping("/")
+    public String login() {
+        return "consulta";
+    }
 }
