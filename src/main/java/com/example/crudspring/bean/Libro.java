45eb7aa65b6cb1e2095d9630cad0c2575006ea01
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-10-12 13:03:40 +0200

    Refactorización de la clase Libro como entidad JPA con anotaciones y atributos mejorados.

--- src/main/java/com/example/crudspring/bean/Libro.java
+++ src/main/java/com/example/crudspring/bean/Libro.java
@@ -1,11 +1,24 @@
 package com.example.crudspring.bean;
 
+import jakarta.persistence.*;
+
+@Entity // Transforma la clase en una entidad de JPA
+@Table(name = "libros") // Indica que la tabla de la base de datos se llama libros
 public class Libro {
+    @Id // Indica que el atributo id es la clave primaria de la tabla
+    @Column(name = "id") // Indica que el atributo id hace referencia a la columna id de la tabla
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // Indica que el atributo id es autoincremental en la base de datos
     private int id;
+    @Column(name = "titulo", nullable = false, length = 30)
     private String titulo;
+    @Column(name = "autor")
     private String autor;
+    @Column(name = "editorial")
     private String editorial;
+    @Column(name = "fecha")
     private String fecha;
+    @Column(name = "tematica")
     private String tematica;
 
     public Libro(int id, String titulo, String autor, String editorial, String fecha, String tematica) {
@@ -15,6 +28,11 @@ public class Libro {
         this.editorial = editorial;
         this.fecha = fecha;
         this.tematica = tematica;
+    }
+
+    // Constructor vacío necesario para que funcione JPA
+    public Libro() {
+
     }
 
     public int getId() {
