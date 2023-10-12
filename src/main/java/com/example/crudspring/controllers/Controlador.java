60a88d00c7e2752cd31530ba5c0377f71abecfc8
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-10-12 10:27:45 +0200

    En el cmetodo login recibimos los datos del formulario y comprobamos si son correctos

--- src/main/java/com/example/crudspring/controllers/Controlador.java
+++ src/main/java/com/example/crudspring/controllers/Controlador.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller // Anotación que indica que esta clase es un controlador de Spring
 @RequestMapping("") // Anotación que indica que este controlador responde a la raíz del servidor
@@ -19,7 +20,13 @@ public class Controlador {
 
     // Responde a la petición POST de la raíz del servidor y devuelve la vista consulta (consulta.html)
     @PostMapping("/")
-    public String login() {
-        return "consulta";
+    public String login(Model model, @RequestParam String nombre, @RequestParam String password) {
+        // @RequestParam es una anotación que indica que el parámetro se pasa por la URL
+        // Tiene que coincidir el nombre del parámetro con el nombre del campo del formulario de la vista
+        if (nombre.equals("edu") && password.equals("1234")) {
+            return "consulta";
+        } else {
+            return "login";
+        }
     }
 }
