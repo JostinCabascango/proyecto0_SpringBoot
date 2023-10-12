00eda8c0bbd1cb471e5d4f1235cd29d70589795e
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-10-12 11:55:45 +0200

    Backend con Arraylist acabado , funciona EL CRUD perfectamente

--- src/main/java/com/example/crudspring/controllers/Controlador.java
+++ src/main/java/com/example/crudspring/controllers/Controlador.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
+
 @Controller // Anotación que indica que esta clase es un controlador de Spring
 @RequestMapping("") // Anotación que indica que este controlador responde a la raíz del servidor
 public class Controlador {
@@ -51,6 +52,7 @@ public class Controlador {
         return "consulta";
     }
 
+    // Responde a la petición GET de la ruta /borrado/{id} y devuelve la vista consulta (consulta.html)
     @GetMapping("/borrado/{id}")
     public String borrar(@PathVariable int id, Model model) {
         bd.borrar(id);
@@ -59,6 +61,33 @@ public class Controlador {
         model.addAttribute("libros", libros);
         model.addAttribute("boton", "insertar");
         model.addAttribute("action", "/insertar");
+        return "consulta";
+    }
+
+    // Responde a la petición GET de la ruta /modificar/{id} y devuelve la vista consulta (consulta.html)
+    @GetMapping("/modificar/{id}")
+    public String modificar(@PathVariable int id, Model model) {
+        Libro libro = bd.getLibro(id); // Obtiene el libro con el id indicado
+        ArrayList<Libro> libros = bd.getLibros();
+        model.addAttribute("usuario", this.usuario);
+        model.addAttribute("libros", libros);
+        model.addAttribute("boton", "Actualizar");
+        model.addAttribute("action", "/modificar");
+        model.addAttribute("libro", libro); // Añade el atributo libro a la vista consulta.html
+        return "consulta";
+    }
+
+    // Responde a la petición POST de la ruta /modificar y devuelve la vista consulta (consulta.html)
+    @PostMapping("/modificar")
+
+    public String modificar(Libro libro, Model model) {
+        bd.modificar(libro);
+        ArrayList<Libro> libros = bd.getLibros();
+        model.addAttribute("usuario", this.usuario);
+        model.addAttribute("libros", libros);
+        model.addAttribute("boton", "insertar");
+        model.addAttribute("action", "/insertar");
+        model.addAttribute("libro", null);
         return "consulta";
     }
 
--- src/main/java/com/example/crudspring/repository/BaseDatos.java
+++ src/main/java/com/example/crudspring/repository/BaseDatos.java
@@ -18,17 +18,17 @@ public class BaseDatos {
         libros.add(new Libro(7, "El Alquimista", "Coelho", "Planeta", "1988", "Novela"));
         libros.add(new Libro(8, "El nombre del viento", "Rothfuss", "Plaza & Janés", "2007", "Fantasía"));
         libros.add(new Libro(9, "El retrato de Dorian Gray", "Wilde", "Alianza", "1890", "Novela"));
-        libros.add(new Libro(10, "El retrato de Dorian Gray", "Wilde", "Alianza", "1890", "Novela"));
-        libros.add(new Libro(11, "El retrato de Dorian Gray", "Wilde", "Alianza", "1890", "Novela"));
-        libros.add(new Libro(12, "El retrato de Dorian Gray", "Wilde", "Alianza", "1890", "Novela"));
-        libros.add(new Libro(13, "El retrato de Dorian Gray", "Wilde", "Alianza", "1890", "Novela"));
-        libros.add(new Libro(14, "El retrato de Dorian Gray", "Wilde", "Alianza", "1890", "Novela"));
-        libros.add(new Libro(15, "El retrato de Dorian Gray", "Wilde", "Alianza", "1890", "Novela"));
-        libros.add(new Libro(16, "El retrato de Dorian Gray", "Wilde", "Alianza", "1890", "Novela"));
-        libros.add(new Libro(17, "El retrato de Dorian Gray", "Wilde", "Alianza", "1890", "Novela"));
-        libros.add(new Libro(18, "El retrato de Dorian Gray", "Wilde", "Alianza", "1890", "Novela"));
-        libros.add(new Libro(19, "El retrato de Dorian Gray", "Wilde", "Alianza", "1890", "Novela"));
-        libros.add(new Libro(20, "El retrato de Dorian Gray", "Wilde", "Alianza", "1890", "Novela"));
+        libros.add(new Libro(10, "1984", "George Orwell", "Secker & Warburg", "1949", "Ciencia Ficción"));
+        libros.add(new Libro(11, "Don Quijote", "Miguel de Cervantes", "Anaya", "1605", "Novela"));
+        libros.add(new Libro(12, "Matar un ruiseñor", "Harper Lee", "J. B. Lippincott & Co.", "1960", "Novela"));
+        libros.add(new Libro(13, "La Odisea", "Homero", "Desconocida", "Siglo VIII a.C.", "Epopeya"));
+        libros.add(new Libro(14, "Crimen y Castigo", "Fiodor Dostoievski", "Editorial El Galeón", "1866", "Novela"));
+        libros.add(new Libro(15, "Drácula", "Bram Stoker", "Archibald Constable and Company", "1897", "Terror"));
+        libros.add(new Libro(16, "Moby-Dick", "Herman Melville", "Richard Bentley", "1851", "Aventura"));
+        libros.add(new Libro(17, "Ulises", "James Joyce", "Sylvia Beach", "1922", "Novela"));
+        libros.add(new Libro(18, "Cien años de soledad", "Gabriel García Márquez", "Sudamericana", "1967", "Novela"));
+        libros.add(new Libro(19, "Harry Potter y la piedra filosofal", "J.K. Rowling", "Bloomsbury", "1997", "Fantasía"));
+
 
     }
 
@@ -50,6 +50,32 @@ public class BaseDatos {
             Libro libro = iterator.next();
             if (libro.getId() == id) {
                 iterator.remove();
+                break;
+            }
+        }
+    }
+
+    public Libro getLibro(int id) {
+        Iterator<Libro> iterator = libros.iterator();
+        while (iterator.hasNext()) {
+            Libro libro = iterator.next();
+            if (libro.getId() == id) {
+                return libro;
+            }
+        }
+        return null;
+    }
+
+    public void modificar(Libro libro) {
+        Iterator<Libro> iterator = libros.iterator();
+        while (iterator.hasNext()) {
+            Libro li = iterator.next();
+            if (libro.getId() == li.getId()) {
+                li.setTitulo(libro.getTitulo());
+                li.setAutor(libro.getAutor());
+                li.setEditorial(libro.getEditorial());
+                li.setFecha(libro.getFecha());
+                li.setTematica(libro.getTematica());
                 break;
             }
         }
