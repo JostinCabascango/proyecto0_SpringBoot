c3c95a2f8549764b9b14c2b07141b025268954fb
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-10-12 10:58:24 +0200

    AAñadidos libros a la vista de consulta

--- src/main/java/com/example/crudspring/controllers/Controlador.java
+++ src/main/java/com/example/crudspring/controllers/Controlador.java
@@ -1,6 +1,8 @@
 package com.example.crudspring.controllers;
 
+import com.example.crudspring.bean.Libro;
 import com.example.crudspring.bean.Usuario;
+import com.example.crudspring.repository.BaseDatos;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +10,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+
 @Controller // Anotación que indica que esta clase es un controlador de Spring
 @RequestMapping("") // Anotación que indica que este controlador responde a la raíz del servidor
 public class Controlador {
+    BaseDatos bd = new BaseDatos();
+
     @GetMapping("/")
     // Responde a la petición GET de la raíz del servidor y devuelve la vista login (login.html)
     public String iniciar(Model model) {
@@ -23,7 +29,9 @@ public class Controlador {
     @PostMapping("/")
     public String login(Usuario usuario, Model model) {
         if (usuario.getNombre().equals("edu") && usuario.getPassword().equals("edu")) {
-            model.addAttribute("usuario", usuario);
+            ArrayList<Libro> libros = bd.getLibros();
+            model.addAttribute("usuario", usuario);  // Añade el atributo usuario a la vista consulta.html
+            model.addAttribute("libros", libros); // Añade el atributo libros a la vista consulta.html
             return "consulta";
         } else {
             model.addAttribute("error", "Credenciales incorrectas. Por favor, inténtalo de nuevo.");
