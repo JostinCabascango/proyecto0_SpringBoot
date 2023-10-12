53dc11359461082e4e745f6e87cfeb0cd3060ac2
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-10-12 13:58:07 +0200

    Inicializacion en h2 -jpa spring

---
+++ src/main/resources/db/schema.sql
@@ -0,0 +1,15 @@
+-- Creación de la tabla 'libros' si no existe
+CREATE TABLE IF NOT EXISTS libros (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(100) NOT NULL,
+    autor VARCHAR(50) NOT NULL,
+    editorial VARCHAR(50) NOT NULL,
+    fecha DATE NOT NULL,
+    tematica VARCHAR(50) NOT NULL
+    );
+-- Creación de la tabla 'usuarios' si no existe
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario VARCHAR(30) NOT NULL UNIQUE
+    password VARCHAR(50) NOT NULL
+    );
